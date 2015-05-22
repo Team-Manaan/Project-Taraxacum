337992243a78498315ba67977858f6eab88ceaca
StanislavGeorgieff <stanislav.georgieff@gmail.com>
2015-05-22 20:46:15 +0300

    Add GameObject.cs

---
+++ ProjectTaraxacum/ProjectTaraxacum/GameObject.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectTaraxacum
+{
+    using System;
+
+    public class GameObject
+    {
+        public int x;
+        public int y;
+        public char c;
+        public ConsoleColor color;
+                                                         // Exstracted the struct and made it into a class!
+        public GameObject()
+        {
+
+        }
+
+        public GameObject(int x, int y, char c, ConsoleColor color) // new constructor!
+        {
+            this.x = x;
+            this.y = y;
+            this.c = c;
+            this.color = color;
+        }
+    }
+}
